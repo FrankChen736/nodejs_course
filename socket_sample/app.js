42ec48f6a98f7d770eb7d0521b13b91f0c8a436f
Frank Chen <frankchen736@gmail.com>
2015-04-22 06:16:47 +0800

    socket.io add nickname

--- socket_sample/app.js
+++ socket_sample/app.js
@@ -6,23 +6,51 @@ app.set('view engine', 'ejs');
 
 app.set('views', 'views');
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
 	res.render('index');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
 	console.log('a user connected');
-	io.emit('chat message', ' a user connected');
-	socket.on('disconnect', function(){
-		console.log('user disconnected');
-		io.emit('chat message', ' a user disconnected');
+
+	console.log(socket.rooms);
+
+	socket.emit('chat message', {
+		from: '系統',
+		content: '請輸入你的暱稱'
 	});
-	socket.on('chat message', function(msg){
-		console.log('message: ' + msg);
-		io.emit('chat message', msg);
-	})
+	socket.on('chat message', function(msg) {
+		if (!socket.name) {
+			console.log(msg);
+			if (msg) {
+				socket.name = msg;
+				io.emit('chat message', {
+					from: socket.name,
+					content: '歡迎[' + socket.name + '] 來到這裡'
+				});
+			} else {
+				socket.emit('chat message', {
+					from: '系統',
+					content: '請輸入你的暱稱'
+				});
+			}
+		} else {
+			socket.broadcast.emit('chat message', {
+				from: socket.name,
+				content: msg
+			});
+		}
+
+		socket.on('disconnect', function() {
+			console.log('user disconnected');
+			io.emit('chat message', {
+				form: '系統',
+				content: 'a user disconnected'
+			});
+		});
+	});
 });
 
-http.listen(3000, function(){
+http.listen(3000, function() {
 	console.log('servre start at port :3000');
 });
